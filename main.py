7fc7c691d32e81e1937bdc9bdc384c5f3470b6b2
Vadim Velikanov <vad.velikanov@yandex.ru>
2025-07-03 21:19:46 +0300

    base template and index page with translation

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+# Configuration for Babel (i18n)
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_SUPPORTED_LOCALES'] = ['en', 'ru']
+
+babel = Babel()
+
+def get_locale():
+    # Allow ?lang=<code> to override
+    lang = request.args.get('lang')
+    if lang in app.config['BABEL_SUPPORTED_LOCALES']:
+        return lang
+    return request.accept_languages.best_match(app.config['BABEL_SUPPORTED_LOCALES'])
+
+babel.init_app(app, locale_selector=get_locale)
+
+@app.context_processor
+def inject_locale():
+    return {'get_locale': get_locale}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 if __name__ == '__main__':
-    pass  # Do something
+    app.run(debug=True)
