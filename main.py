dd2e42ad6e1466e8244cc06449210e0961e6ef9f
kust <vad.velikanov@yandex.ru>
2025-07-05 16:45:46 +0300

    submit page prototype

--- main.py
+++ main.py
@@ -25,5 +25,9 @@ def inject_locale():
 def index():
     return render_template('index.html')
 
+@app.route("/submit")
+def submit():
+    return render_template("submit.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
