28f22512d8876adb2b80496b6467ddbaa4dd3865
kust <vad.velikanov@yandex.ru>
2025-08-21 13:39:09 +0300

    issues iframe searchbar

--- static/admin_panel.js
+++ static/admin_panel.js
@@ -7,3 +7,12 @@ document.querySelectorAll(".clickable-row").forEach(row => {
       window.location = row.dataset.href;
     });
 });
+
+document.getElementById("issueSearch").addEventListener("keyup", function() {
+  let filter = this.value.toLowerCase();
+  let items = document.querySelectorAll("#issuesList a");
+  items.forEach(function(item) {
+    let text = item.textContent.toLowerCase();
+    item.style.display = text.includes(filter) ? "" : "none";
+  });
+});
